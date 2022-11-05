2f116a53493b11d05e6d3c99d06f1d826c317010
Abigtime <115564855+Abigtime@users.noreply.github.com>
2022-11-05 16:23:11 +0300

    Программа 4. Показать частное. Новый тип данных double. Введна новая функция Random

--- ex004_Div/Program.cs
+++ ex004_Div/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Программа 4. Показать частное 
-double numberA=3;
-double numberB=2;
+double numberA=new Random().Next(1,10); //случайное число 1,2,3,4,5,6,7,8,9
+Console.WriteLine(numberA);
+double numberB=new Random().Next(1,10); //случайное число 1,2,3,4,5,6,7,8,9
+Console.WriteLine(numberB);
 double resulte=numberA/numberB;
-Console.Write(resulte);
+Console.WriteLine(resulte);
