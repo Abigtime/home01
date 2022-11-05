4125b9bb87dc912eaf61b4978bb877e58e4ee59b
Abigtime <115564855+Abigtime@users.noreply.github.com>
2022-11-05 15:59:28 +0300

    Программа 4. Показать частное. Новый тип данных double

--- ex004_Div/Program.cs
+++ ex004_Div/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Программа 4. Показать частное 
-int numberA=3;
-int numberB=5;
-int resulte=numberA/numberB;
+double numberA=3;
+double numberB=2;
+double resulte=numberA/numberB;
 Console.Write(resulte);
