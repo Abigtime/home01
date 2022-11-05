e9672140a1d3c7c7f83c3bf272e8cc865a705c59
Abigtime <115564855+Abigtime@users.noreply.github.com>
2022-11-05 17:00:00 +0300

    программа 5. Привет Маша и прочие Юзеры

---
+++ ex005_HelloMasha/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа 5. Приветствовать Машу и прочих юзеров 
+Console.WriteLine("Введите имя пользователя:");//ввод любого имени, включая Маша
+string username=Console.ReadLine(); 
+if (username=="Маша")
+    { 
+        Console.WriteLine("Ура, это же Маша!");    
+    }
+        
+else
+    {
+        Console.Write("Привет, ");
+        Console.WriteLine(username);        
+    }
+        
+    
